93c49e47058bd86478e3626d0caddaf9a40bbcde
JuanjoSquare1 <juanjo@square1.io>
2020-03-17 09:18:45 +0100

    MainNavigation
    Main navigation implementation

--- app/src/main/kotlin/io/square1/limor/di/modules/ActivitiesModule.kt
+++ app/src/main/kotlin/io/square1/limor/di/modules/ActivitiesModule.kt
@@ -3,8 +3,10 @@ package io.square1.limor.di.modules
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import io.square1.limor.di.modules.fragments.CommonFragmentsBuildersModule
+import io.square1.limor.di.modules.fragments.MainActivityFragmentsBuildersModule
 import io.square1.limor.di.modules.fragments.SignActivityFragmentsBuildersModule
 import io.square1.limor.scenes.authentication.SignActivity
+import io.square1.limor.scenes.main.MainActivity
 import io.square1.limor.scenes.splash.SplashActivity
 
 @Module
@@ -18,5 +20,11 @@ abstract class ActivitiesModule {
     ])
     abstract fun contributeSignActivityInjector(): SignActivity
 
+
+    @ContributesAndroidInjector(modules = [
+        (CommonFragmentsBuildersModule::class),
+        (MainActivityFragmentsBuildersModule::class)
+    ])
+    abstract fun contributeMainActivityInjector(): MainActivity
 
 }
