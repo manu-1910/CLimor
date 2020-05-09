7f3fc99a2fab0be32d0b6058a62f60155df795e0
JuanjoSquare1 <juanjo@square1.io>
2020-05-09 07:46:46 +0200

    Drafts list
    Adapter and viewholder item draft before play audio implementation

--- app/src/main/kotlin/io/square1/limor/scenes/main/adapters/DraftAdapter.kt
+++ app/src/main/kotlin/io/square1/limor/scenes/main/adapters/DraftAdapter.kt
@@ -2,37 +2,71 @@ package io.square1.limor.scenes.main.adapters
 
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import io.square1.limor.R
 import io.square1.limor.uimodels.UIDraft
+import org.jetbrains.anko.sdk23.listeners.onClick
+import java.util.*
 
 class DraftAdapter(
-    private val draftList: ArrayList<UIDraft>,
-    private val listener: OnItemClickListener?,
-    private val secondaryInformationListener: OnSecondaryInformationListenerClickListener?
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val draftsList: ArrayList<UIDraft>,
+    private val listener: OnItemClickListener,
+    private val deleteListener: OnDeleteItemClickListener
+) : RecyclerView.Adapter<DraftAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return DraftViewHolder(LayoutInflater.from(parent.context), parent)
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        //Item components
+        val tvDraftTitle: TextView = itemView.findViewById(R.id.tvDraftTitle)
+        val tvDraftDescription: TextView = itemView.findViewById(R.id.tvDraftDescription)
+        val ivDraftDelete: ImageView = itemView.findViewById(R.id.ivDraftDelete)
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val centreViewHolder: DraftViewHolder = holder as DraftViewHolder
-        val uiCentre: UIDraft = draftList[position]
-        centreViewHolder.bind(uiCentre, secondaryInformationListener, position)
-        centreViewHolder.itemView.setOnClickListener {
-            listener?.onItemClick(
-                draftList[position], position)
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): DraftAdapter.ViewHolder {
+        return DraftAdapter.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_drafts_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return draftsList.size
+    }
+
+    override fun onBindViewHolder(holder: DraftAdapter.ViewHolder, position: Int) {
+
+        holder.tvDraftTitle.text = draftsList[position].title
+        holder.tvDraftDescription.text = draftsList[position].caption
+        holder.itemView.setOnClickListener { listener.onItemClick(draftsList[position]) }
+
+        if (draftsList[position].isEditMode!!) {
+            holder.ivDraftDelete.setImageResource(R.drawable.delete_symbol)
+            holder.ivDraftDelete.onClick { deleteListener.onDeleteItemClick(position) }
+        } else {
+            holder.ivDraftDelete.setImageResource(android.R.color.transparent)
         }
+
     }
 
-    override fun getItemCount(): Int = draftList.size
 
     interface OnItemClickListener {
-        fun onItemClick(item: UIDraft, position: Int)
+        fun onItemClick(item: UIDraft)
     }
 
-    interface OnSecondaryInformationListenerClickListener {
-        fun onSecondaryInformationClick(item: UIDraft, position: Int)
+    interface OnDeleteItemClickListener {
+        fun onDeleteItemClick(position: Int)
     }
+
 }
+
+
