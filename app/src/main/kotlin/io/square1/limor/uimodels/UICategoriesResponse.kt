da98991d354c1f6bae93fe8a09f5a9de234247fd
Jose Valdés <jose.v@square1.io>
2020-09-18 09:52:01 +0200

    Fixed

--- app/src/main/kotlin/io/square1/limor/uimodels/UICategoriesResponse.kt
+++ app/src/main/kotlin/io/square1/limor/uimodels/UICategoriesResponse.kt
@@ -1,15 +1,17 @@
 package io.square1.limor.uimodels
 
+import java.io.Serializable
+
 
 data class UICategoriesResponse(
     val code: Int,
     val message: String,
     val data: UICategoriesArray
-)
+) : Serializable
 
 data class UICategoriesArray(
     val categories: ArrayList<UICategory>
-)
+) : Serializable
 
 class UICategory(
     val id: Int,
@@ -17,4 +19,4 @@ class UICategory(
     val priority: Int,
     val created_at: Long,
     val updated_at: Long
-)
+) : Serializable
