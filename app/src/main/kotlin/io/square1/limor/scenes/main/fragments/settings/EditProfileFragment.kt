a0cba8a57943713c999f5b61fbbca750d2241c68
JuanjoSquare1 <juanjo@square1.io>
2020-10-15 17:53:13 +0200

    Update manage of enable_notifications
    Fix updated

--- app/src/main/kotlin/io/square1/limor/scenes/main/fragments/settings/EditProfileFragment.kt
+++ app/src/main/kotlin/io/square1/limor/scenes/main/fragments/settings/EditProfileFragment.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -164,7 +163,7 @@ class EditProfileFragment : BaseFragment() {
         updateUserViewModel.phone_number = etPhone.text.toString()
         updateUserViewModel.date_of_birth =  ageToTimestamp(etAge.text.toString().toInt())
         updateUserViewModel.gender = etGender.text.toString()
-        updateUserViewModel.notifications_enabled = sessionManager.getPushNotificationsEnabled()!!
+        updateUserViewModel.notifications_enabled = sessionManager.getStoredUser()!!.notifications_enabled
         if(profileHasImage){
             updateUserViewModel.image = profileImageUrlFinal
         }
@@ -202,6 +201,7 @@ class EditProfileFragment : BaseFragment() {
             handleOnApiError(it)
         })
     }
+
 
     private fun handleOnApiError(it: UIErrorResponse) {
         if (app!!.merlinsBeard!!.isConnected) {
@@ -360,8 +360,6 @@ class EditProfileFragment : BaseFragment() {
             callToApiUpdateUser()
         }
     }
-
-
 
 
 }
