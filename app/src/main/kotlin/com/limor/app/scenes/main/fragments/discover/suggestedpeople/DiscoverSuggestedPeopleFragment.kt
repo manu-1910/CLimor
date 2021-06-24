85cb1a84056d58ace5d0c61e06eab68e82b99c3b
Oleksandr Katrych <alex@awebdigital.co>
2021-06-24 16:28:56 +0300

    Create DiscoverSuggestedPeopleFragment

---
+++ app/src/main/kotlin/com/limor/app/scenes/main/fragments/discover/suggestedpeople/DiscoverSuggestedPeopleFragment.kt
@@ -0,0 +1,45 @@
+package com.limor.app.scenes.main.fragments.discover.suggestedpeople
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.limor.app.R
+import com.limor.app.common.BaseFragment
+import com.limor.app.databinding.FragmentDiscoverSuggestedPeopleBinding
+import javax.inject.Inject
+
+class DiscoverSuggestedPeopleFragment: BaseFragment() {
+
+    private var _binding: FragmentDiscoverSuggestedPeopleBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel: DiscoverSuggestedPeopleViewModel by viewModels { viewModelFactory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDiscoverSuggestedPeopleBinding.inflate(inflater, container, false)
+        initViews()
+        subscribeForEvents()
+        return binding.root
+    }
+
+    private fun initViews() {
+        binding.toolbar.title.setText(R.string.suggested_people)
+        binding.toolbar.btnBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+    }
+
+    private fun subscribeForEvents() {
+        TODO()
+    }
+}
---
+++ app/src/main/kotlin/com/limor/app/scenes/main/fragments/discover/suggestedpeople/DiscoverSuggestedPeopleViewModel.kt
@@ -0,0 +1,10 @@
+package com.limor.app.scenes.main.fragments.discover.suggestedpeople
+
+import androidx.lifecycle.ViewModel
+
+class DiscoverSuggestedPeopleViewModel(
+    // some repo to get suggested people
+): ViewModel() {
+
+
+}
