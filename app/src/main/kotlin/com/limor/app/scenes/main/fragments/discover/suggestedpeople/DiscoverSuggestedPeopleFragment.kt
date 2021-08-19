78fae6f71039c86f3ef878de07874838e9307d1a
Vasil <87858071+vasiltab@users.noreply.github.com>
2021-08-19 10:47:22 +0300

    opens the notification when clicking the notification icon (#115)

--- app/src/main/kotlin/com/limor/app/scenes/main/fragments/discover/suggestedpeople/DiscoverSuggestedPeopleFragment.kt
+++ app/src/main/kotlin/com/limor/app/scenes/main/fragments/discover/suggestedpeople/DiscoverSuggestedPeopleFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.limor.app.R
 import com.limor.app.common.BaseFragment
 import com.limor.app.databinding.FragmentDiscoverSuggestedPeopleBinding
@@ -45,6 +46,10 @@ class DiscoverSuggestedPeopleFragment : BaseFragment() {
         suggestedPeopleAdapter.setOnItemClickListener { item, view ->
             val person = (item as SuggestedPersonBigItem).person
             // TODO
+        }
+
+        binding.toolbar.btnNotification.setOnClickListener {
+            findNavController().navigate(R.id.navigation_notifications)
         }
     }
 
