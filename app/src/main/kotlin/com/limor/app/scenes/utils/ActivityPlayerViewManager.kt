104dba4a82a67e9947a0c1c341838dcc4d0cbd52
Vasil <87858071+vasiltab@users.noreply.github.com>
2021-08-23 09:05:05 +0300

    uses different motion layout callbacks to manage mini vs extended player display (#125)
    this avoids the case where the mini player displays the extended player

--- app/src/main/kotlin/com/limor/app/scenes/utils/ActivityPlayerViewManager.kt
+++ app/src/main/kotlin/com/limor/app/scenes/utils/ActivityPlayerViewManager.kt
@@ -11,7 +11,6 @@ import com.limor.app.scenes.main_new.fragments.ExtendedPlayerFragment
 import com.limor.app.scenes.main_new.fragments.SmallPlayerFragment
 import com.limor.app.service.PlayerBinder
 import timber.log.Timber
-import kotlin.math.abs
 
 class ActivityPlayerViewManager(
     private val fragmentManager: FragmentManager,
@@ -22,7 +21,6 @@ class ActivityPlayerViewManager(
     private var currentFragment: Fragment? = null
     private var currentArgs: PlayerViewManager.PlayerArgs? = null
     private var isPlayerVisible: Boolean = false
-    private var lastTransitionProgress = 0f
 
     init {
         playerBinding.motionLayout.apply {
@@ -61,47 +59,69 @@ class ActivityPlayerViewManager(
         stop()
     }
 
-    override fun onTransitionChange(
-        motionLayout: MotionLayout,
-        startId: Int,
-        endId: Int,
-        progress: Float
-    ) {
-        if (progress - lastTransitionProgress > 0) {
-            // from start to end
-            val atEnd = abs(progress - 1f) < 0.9f
-            if (atEnd && currentFragment !is ExtendedPlayerFragment) {
-                val transaction = fragmentManager.beginTransaction()
-                transaction
-                    .setCustomAnimations(R.animator.show, 0)
-                currentFragment = ExtendedPlayerFragment.newInstance(currentArgs!!.castId, true).also {
-                    transaction
-                        .replace(playerBinding.playerContainer.id, it)
-                        .commit()
-                }
-            }
-        } else {
-            // from end to start
-            val atStart = progress < 0.2f
-            if (atStart && currentFragment !is SmallPlayerFragment) {
-                val transaction = fragmentManager.beginTransaction()
-                transaction
-                    .setCustomAnimations(R.animator.show, 0)
-                currentFragment = SmallPlayerFragment.newInstance(currentArgs!!.castId).also {
-                    transaction
-                        .replace(playerBinding.playerContainer.id, it)
-                        .commit()
-                }
-            }
-        }
-        lastTransitionProgress = progress
-    }
-
     fun stop() {
         playerBinder.stop()
     }
 
-    override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
-    override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) = Unit
+    private inline fun <reified T> showFragment(fragment: () -> Fragment) {
+        if (currentFragment is T) {
+            return
+        }
+        val targetFragment = fragment()
+        currentFragment = fragment()
+        fragmentManager.beginTransaction().also {
+            it.setCustomAnimations(R.animator.show, 0)
+            it.replace(playerBinding.playerContainer.id, targetFragment)
+            it.commit()
+        }
+    }
+
+    private fun showExtendedPlayer() {
+        showFragment<ExtendedPlayerFragment> {
+            ExtendedPlayerFragment.newInstance(currentArgs!!.castId, true)
+        }
+    }
+
+    private fun showMiniPlayer(){
+        showFragment<SmallPlayerFragment> {
+            SmallPlayerFragment.newInstance(currentArgs!!.castId)
+        }
+    }
+
+    private fun toggleFragments(shouldShowMiniPlayer: Boolean) {
+        if (shouldShowMiniPlayer) {
+            showMiniPlayer()
+        } else {
+            showExtendedPlayer()
+        }
+    }
+
+    override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+        val isAtStart = currentId == R.id.start
+        // "at start" means the bottom mini player, check R.xml.container_transition_player_scene
+        // where the @+id/start ConstraintSet has the mini player height and position, so when the
+        // currentId is the "start" ID we need to be showing the mini player
+        toggleFragments(isAtStart)
+    }
+
+    override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+        // All transitions should always start with the extended player, there are these 2 cases:
+        // 1. from mini player -> extended player
+        // 2. from extended player -> mini player
+        //
+        // Case #2 is clear, it should be showing the maximized extended player and move to the
+        // mini player bounds until the animation is completed at which point the other motion
+        // callback (onTransitionCompleted) will ensure the mini player would be shown.
+        //
+        // Case #1 too needs to start with the Extended player. This is the case when the mini
+        // player is shown and we need to maximize it to the extended player. For effect we
+        // immediately show the extended player and let it maximize to its bounds. So there too
+        // the start fragment should be the Extended player.
+        //
+        // Bottom-line: always start from the ExtendedPlayerFragment
+        toggleFragments(false)
+    }
+
+    override fun onTransitionChange(motionLayout: MotionLayout, startId: Int, endId: Int, progress: Float) = Unit
     override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
 }
