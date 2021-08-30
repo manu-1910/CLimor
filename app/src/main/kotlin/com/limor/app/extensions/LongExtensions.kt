e69c0a833cd5d0268dc7f5e9b2b63e52d163a3b1
Vasil <87858071+vasiltab@users.noreply.github.com>
2021-08-30 12:42:25 +0300

    fixes a bug where a value of zero would crash the app because log10 of zero is undefined (#154)
    represented as -∞

--- app/src/main/kotlin/com/limor/app/extensions/LongExtensions.kt
+++ app/src/main/kotlin/com/limor/app/extensions/LongExtensions.kt
@@ -1,13 +1,18 @@
 package com.limor.app.extensions
 
-import java.lang.Math.log10
+import kotlin.math.log10
 import kotlin.math.pow
 
 val Long.formatHumanReadable: String
-    get() = log10(toDouble()).toInt().div(3).let {
-        val precision = when (it) {
-            0 -> 0; else -> 1
+    get() {
+        if (this < 1000) {
+            return "$this"
         }
-        val suffix = arrayOf("", "k", "m", "g", "t", "p", "e", "z", "y")
-        String.format("%.${precision}f${suffix[it]}", toDouble() / 10.0.pow(it * 3))
+        return log10(toDouble()).toInt().div(3).let {
+            val precision = when (it) {
+                0 -> 0; else -> 1
+            }
+            val suffix = arrayOf("", "k", "m", "g", "t", "p", "e", "z", "y")
+            String.format("%.${precision}f${suffix[it]}", toDouble() / 10.0.pow(it * 3))
+        }
     }
