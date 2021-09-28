7317a7542dc1a317723e4a356cf6514bc68c553b
Vasil <87858071+vasiltab@users.noreply.github.com>
2021-09-28 08:51:53 +0300

    Bugfix: LMR-1249 - edit comment persistence (#223)
    * adds utils func to clear the comment action
    
    * adds actionsViewMOdel to the base class
    
    * resets the comment action after editing a comment
    
    * removes subclass models as the base class already contains a model ref

--- app/src/main/kotlin/com/limor/app/scenes/main_new/fragments/comments/BaseCommentsFragment.kt
+++ app/src/main/kotlin/com/limor/app/scenes/main_new/fragments/comments/BaseCommentsFragment.kt
@@ -3,6 +3,7 @@ package com.limor.app.scenes.main_new.fragments.comments
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.limor.app.BuildConfig
@@ -11,6 +12,7 @@ import com.limor.app.common.BaseFragment
 import com.limor.app.common.Constants
 import com.limor.app.scenes.auth_new.util.PrefsHandler
 import com.limor.app.scenes.main.viewmodels.CommentsViewModel
+import com.limor.app.scenes.main.viewmodels.HandleCommentActionsViewModel
 import com.limor.app.scenes.main_new.fragments.mentions.UserMentionPopup
 import com.limor.app.scenes.main_new.view_model.UserMentionViewModel
 import com.limor.app.scenes.utils.Commons
@@ -33,7 +35,9 @@ abstract class UserMentionFragment : BaseFragment(), UserMentionPopup.UserMentio
 
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private val userModel: UserMentionViewModel by viewModels { viewModelFactory }
+
     protected val commentsViewModel: CommentsViewModel by viewModels { viewModelFactory }
+    protected val actionsViewModel: HandleCommentActionsViewModel by activityViewModels { viewModelFactory }
 
     protected var textAndVoiceInput: TextAndVoiceInput? = null
 
@@ -71,6 +75,7 @@ abstract class UserMentionFragment : BaseFragment(), UserMentionPopup.UserMentio
 
     protected fun editComment(comment: CommentUIModel) {
         textAndVoiceInput?.edit(comment)
+        actionsViewModel.resetCommentAction()
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
