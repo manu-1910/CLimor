ae8cacf4d9d053270235b2880f3bf87965fc4cb4
Vasil Tabakov <vasil@awebdigital.co>
2021-10-07 11:08:50 +0300

    initial chat repo

---
+++ app/src/main/kotlin/com/limor/app/data/ChatRepository.kt
@@ -0,0 +1,36 @@
+package com.limor.app.data
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ChatRepository @Inject constructor(private val chatDao: ChatDao) {
+
+    fun getSessions() = chatDao.getSessions()
+
+    fun getChat(sessionId: Int) = chatDao.getChat(sessionId)
+
+    private fun addMessage(chatMessage: ChatMessage) = chatDao.insertMessage(chatMessage)
+
+    fun addOtherMessage(content: String, session: ChatSession) {
+        ChatMessage(
+            0,
+            session.id,
+            session.chatUserId,
+            content
+        ).also {
+            addMessage(it)
+        }
+    }
+
+    fun addMyMessage(content: String, session: ChatSession) {
+        ChatMessage(
+            0,
+            session.id,
+            null,
+            content
+        ).also {
+            addMessage(it)
+        }
+    }
+}
