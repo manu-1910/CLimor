afb85cf144eddd0feda6eb254bf83e22590f96a4
Vasil Tabakov <vasil@awebdigital.co>
2021-10-07 18:03:37 +0300

    initial chat session view model

---
+++ app/src/main/kotlin/com/limor/app/dm/ChatSessionViewModel.kt
@@ -0,0 +1,16 @@
+package com.limor.app.dm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import javax.inject.Inject
+
+class ChatSessionViewModel @Inject constructor(
+    chatRepository: ChatRepository,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    val sessions: LiveData<List<ChatSessionWithUser>> = chatRepository.getSessions().asLiveData()
+
+}
