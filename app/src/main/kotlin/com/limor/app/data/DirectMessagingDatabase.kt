249a9b042769a5b8428e1f3a11808fa421bae380
Vasil Tabakov <vasil@awebdigital.co>
2021-10-07 10:59:31 +0300

    adds dao to db

--- app/src/main/kotlin/com/limor/app/data/DirectMessagingDatabase.kt
+++ app/src/main/kotlin/com/limor/app/data/DirectMessagingDatabase.kt
@@ -1,11 +1,15 @@
 package com.limor.app.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.limor.app.common.Constants
 
+@Database(entities = [ChatUser::class, ChatMessage::class, ChatSession::class], version = 1)
 abstract class DirectMessagingDatabase : RoomDatabase() {
+
+    abstract fun chatDao(): ChatDao
 
     companion object {
 
