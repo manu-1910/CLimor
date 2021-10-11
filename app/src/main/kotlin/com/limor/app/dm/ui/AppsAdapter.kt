5963ff3cacdbc59ff5e05bbb262dac44451b45b7
Vasil Tabakov <vasil@awebdigital.co>
2021-10-11 20:58:21 +0300

    implements external share

--- app/src/main/kotlin/com/limor/app/dm/ui/AppsAdapter.kt
+++ app/src/main/kotlin/com/limor/app/dm/ui/AppsAdapter.kt
@@ -9,8 +9,22 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.limor.app.R
 import org.jetbrains.anko.image
+import androidx.core.content.ContextCompat.startActivity
 
-class AppsAdapter(private val apps: List<ResolveInfo>) :
+import android.content.Intent
+
+import android.content.ComponentName
+import android.content.Context
+import com.limor.app.uimodels.CastUIModel
+import org.greenrobot.eventbus.EventBus
+
+
+class AppsAdapter(
+    private val context: Context,
+    private val apps: List<ResolveInfo>,
+    private val podcast: CastUIModel,
+    private val shortLink: String
+) :
     RecyclerView.Adapter<AppsAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -27,12 +41,32 @@ class AppsAdapter(private val apps: List<ResolveInfo>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.image.context.packageManager.let { pm ->
-            apps[position].let { ri ->
-                holder.name.text = ri.loadLabel(pm)
-                holder.image.image = ri.loadIcon(pm)
-            }
+        val ri = apps[position]
+
+        context.packageManager.let { pm ->
+            holder.name.text = ri.loadLabel(pm)
+            holder.image.image = ri.loadIcon(pm)
         }
+        holder.itemView.setOnClickListener {
+            openShareIntent(ri)
+        }
+    }
+
+    private fun openShareIntent(ri: ResolveInfo) {
+        val ai = ri.activityInfo
+        val name = ComponentName(ai.applicationInfo.packageName, ai.name)
+
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            component = name
+
+            putExtra(Intent.EXTRA_SUBJECT, podcast.title)
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.share_message__with_format, shortLink))
+        }
+
+        context.startActivity(sendIntent)
+        ShareDialog.DismissEvent.dismiss()
     }
 
     override fun getItemCount() = apps.size
