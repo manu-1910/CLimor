059f6a6aa330ad07b69f0c2b0646ee46044f9f5d
Vasil Tabakov <vasil@awebdigital.co>
2021-10-11 11:14:56 +0300

    proxy data class for user data

--- app/src/main/kotlin/com/limor/app/dm/LeanUser.kt
+++ app/src/main/kotlin/com/limor/app/dm/LeanUser.kt
@@ -1,4 +1,8 @@
 package com.limor.app.dm
 
-class LeanUser {
-}
+data class LeanUser(
+    val limorUserId: Int,
+    val userName: String?,
+    val displayName: String?,
+    val profileUrl: String?,
+)
