fc73f1aa6f76694c6f79cf2d97c8acc2ce013ead
Vasil Tabakov <vasil@awebdigital.co>
2021-10-12 20:09:36 +0300

    chat target util model

---
+++ app/src/main/kotlin/com/limor/app/dm/ChatTarget.kt
@@ -0,0 +1,26 @@
+package com.limor.app.dm
+
+import com.limor.app.SearchFollowersQuery
+
+data class ChatTarget(
+    val limorUserId: Int,
+    val limorProfileUrl: String?,
+    val limorUserName: String?,
+    val limorDisplayName: String?,
+) {
+    companion object {
+        fun fromSearch(follower: SearchFollowersQuery.SearchFollower) = ChatTarget(
+            limorUserId = follower.id!!,
+            limorUserName = follower.username,
+            limorDisplayName = if (follower.first_name.isNullOrEmpty() && follower.last_name.isNullOrEmpty()) follower.username else "${follower.first_name} ${follower.last_name}",
+            limorProfileUrl = follower.images?.medium_url ?: follower.images?.large_url ?: follower.images?.original_url ?: follower.images?.small_url
+        )
+
+        fun fromSession(sessionWithUser: ChatSessionWithUser) = ChatTarget(
+            limorUserId = sessionWithUser.user.limorUserId,
+            limorUserName = sessionWithUser.user.limorUserName,
+            limorDisplayName = sessionWithUser.user.limorDisplayName,
+            limorProfileUrl = sessionWithUser.user.limorProfileUrl
+        )
+    }
+}
