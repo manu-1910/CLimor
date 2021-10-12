327586cb34b131c2274e8574f8fe2fffddd51678
Vasil Tabakov <vasil@awebdigital.co>
2021-10-12 20:10:15 +0300

    session model now can search in the users chat sessions or followers

--- app/src/main/kotlin/com/limor/app/dm/SessionsViewModel.kt
+++ app/src/main/kotlin/com/limor/app/dm/SessionsViewModel.kt
@@ -1,16 +1,67 @@
 package com.limor.app.dm
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
+import com.limor.app.apollo.GeneralInfoRepository
+import com.limor.app.uimodels.UserUIModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SessionsViewModel @Inject constructor(
-    chatRepository: ChatRepository,
-    private val savedStateHandle: SavedStateHandle
+    private val chatRepository: ChatRepository,
+    private val generalInfoRepository: GeneralInfoRepository,
 ) : ViewModel() {
 
     val sessions: LiveData<List<ChatSessionWithUser>> = chatRepository.getSessions().asLiveData()
 
+    private var _chatTargets = MutableLiveData<List<ChatTarget>>()
+    val chatTargets: LiveData<List<ChatTarget>>
+        get() = _chatTargets
+
+    fun searchFollowers(searchTerm: String) {
+        println("Will search for $searchTerm")
+
+        if (searchTerm.isEmpty()) {
+            _chatTargets.postValue(emptyList())
+            return
+        }
+
+        viewModelScope.launch {
+            try {
+
+                val sessions = chatRepository
+                    .searchSession(searchTerm.lowercase())
+                    .map(ChatTarget::fromSession)
+
+                val targets = mutableListOf<ChatTarget>().apply {
+                    addAll(sessions)
+                }
+
+                println("Sessions -> $sessions")
+
+                val followers = generalInfoRepository.searchFollowers(
+                    term = searchTerm,
+                    limit = Int.MAX_VALUE,
+                    offset = 0
+                )
+
+                print("Search followers -> $followers")
+
+                followers.filterNotNull()
+                    .map(ChatTarget::fromSearch)
+                    .filter { searchTarget ->
+                        targets.none { chatTarget ->
+                            chatTarget.limorUserId == searchTarget.limorUserId
+                        }
+                    }.also { searchTargets ->
+                        targets.addAll(searchTargets)
+                    }
+
+                _chatTargets.postValue(targets)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _chatTargets.postValue(emptyList())
+            }
+        }
+    }
 }
