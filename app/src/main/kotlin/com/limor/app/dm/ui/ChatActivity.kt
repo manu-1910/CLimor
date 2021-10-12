39115153f32bc44aa4efbe646c1e0da8a351b908
Vasil Tabakov <vasil@awebdigital.co>
2021-10-12 23:45:04 +0300

    initial chat activity

--- app/src/main/kotlin/com/limor/app/dm/ui/ChatActivity.kt
+++ app/src/main/kotlin/com/limor/app/dm/ui/ChatActivity.kt
@@ -1,4 +1,35 @@
 package com.limor.app.dm.ui
 
-class ChatActivity {
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.limor.app.R
+import dagger.android.AndroidInjection
+import javax.inject.Inject
+
+class ChatActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        AndroidInjection.inject(this)
+        setContentView(R.layout.activity_chat)
+    }
+
+    companion object {
+        const val KEY_LIMOR_USER_ID = "KEY_LIMOR_USER_ID"
+
+        fun start(context: Context, limorUserId: Int) {
+            Intent(context, ChatActivity::class.java).apply {
+                putExtra(KEY_LIMOR_USER_ID, limorUserId)
+            }.also {
+                context.startActivity(it)
+            }
+        }
+    }
 }
