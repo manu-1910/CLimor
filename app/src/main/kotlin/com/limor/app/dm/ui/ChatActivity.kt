7c5d943843ea0ab4c92320df4f3f7cc276c2b52a
Vasil Tabakov <vasil@awebdigital.co>
2021-10-13 06:57:21 +0300

    chat activity initial chat handling

--- app/src/main/kotlin/com/limor/app/dm/ui/ChatActivity.kt
+++ app/src/main/kotlin/com/limor/app/dm/ui/ChatActivity.kt
@@ -3,22 +3,149 @@ package com.limor.app.dm.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
-import com.limor.app.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.limor.app.databinding.ActivityChatBinding
+import com.limor.app.dm.ChatSessionWithUser
+import com.limor.app.dm.ChatWithData
+import com.limor.app.dm.SessionsViewModel
+import com.limor.app.extensions.loadCircleImage
 import dagger.android.AndroidInjection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ChatActivity : AppCompatActivity() {
 
+    private var coroutineJob: Job = Job()
+    private val scope = CoroutineScope(Dispatchers.IO + coroutineJob)
+
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val chat: SessionsViewModel by viewModels { viewModelFactory }
+
+    private lateinit var binding: ActivityChatBinding
+    private var hasSetHeader = false;
+
+    private var chatAdapter: ChatAdapter? = null
+    private var chatSession: ChatSessionWithUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         AndroidInjection.inject(this)
-        setContentView(R.layout.activity_chat)
+
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setViews()
+
+        getSession()
+    }
+
+    private fun getSession() {
+        if (!intent.hasExtra(KEY_LIMOR_USER_ID)) {
+            finish()
+            return
+        }
+
+        val limorUserId = intent.extras?.getInt(KEY_LIMOR_USER_ID)
+        if (limorUserId == null) {
+            finish()
+            return
+        }
+
+        scope.launch {
+            getChat(limorUserId)
+        }
+    }
+
+    private suspend fun getChat(limorUserId: Int) {
+        val data = chat.getChat(limorUserId)
+        if (data == null) {
+            reportGenericError()
+        } else {
+            observeChat(data)
+        }
+    }
+
+    private fun observeChat(data: LiveData<ChatWithData>) {
+        lifecycleScope.launch {
+            data.observe(this@ChatActivity) { onChatData(it) }
+        }
+    }
+
+    private fun reportGenericError() {
+        Toast.makeText(this, "Could not start chat. Please try later.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onChatData(chatData: ChatWithData) {
+        println("ZZZZ Got chat data with ${chatData.messages.size} messages")
+
+        if (null == chatAdapter) {
+            chatAdapter = ChatAdapter(
+                this,
+                chatData
+            )
+            binding.recyclerChat.adapter = chatAdapter
+        } else {
+            chatAdapter?.apply {
+                setChatData(chatData)
+                notifyDataSetChanged()
+            }
+        }
+
+        if (!hasSetHeader) {
+            hasSetHeader = true;
+            setHeader(chatData)
+        }
+    }
+
+    private fun setHeader(chatData: ChatWithData) {
+        chatData.sessionWithUser.user.let {
+            binding.textTitle.text = it.limorDisplayName
+            binding.profile.loadCircleImage(it.limorProfileUrl)
+        }
+    }
+
+    private fun setViews() {
+        binding.buttonBack.setOnClickListener {
+            finish()
+        }
+        (binding.recyclerChat.layoutManager as? LinearLayoutManager)?.reverseLayout = true
+
+        binding.editMessageText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val enable = s.isNotEmpty()
+                binding.buttonSendMessage.apply {
+                    isEnabled = enable
+                    isActivated = enable
+                }
+            }
+        })
+
+        binding.buttonSendMessage.setOnClickListener {
+            val session = chatSession ?: return@setOnClickListener
+            chat.addMyMessage(session, binding.editMessageText.text.toString())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineJob.cancel()
     }
 
     companion object {
