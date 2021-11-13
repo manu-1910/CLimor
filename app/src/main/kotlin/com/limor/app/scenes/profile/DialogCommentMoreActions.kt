ef4c43f2ff2bad9f8da2fb287f4b3b55e8d4fe58
Vasil Tabakov <vasil@awebdigital.co>
2021-11-13 16:26:38 +0200

    refactored the comment actions dialog for generic use

--- app/src/main/kotlin/com/limor/app/scenes/profile/DialogCommentMoreActions.kt
+++ app/src/main/kotlin/com/limor/app/scenes/profile/DialogCommentMoreActions.kt
@@ -7,55 +7,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import com.limor.app.R
+import com.limor.app.BuildConfig
 import com.limor.app.databinding.DialogCommentMoreActionsBinding
-import com.limor.app.databinding.DialogOtherUserActionsBinding
-import com.limor.app.scenes.main.fragments.profile.UserProfileActivity
-import com.limor.app.scenes.main.fragments.profile.UserProfileViewModel
-import com.limor.app.scenes.main.fragments.settings.SettingsActivity
+import com.limor.app.scenes.auth_new.util.PrefsHandler
 import com.limor.app.scenes.main.viewmodels.CommentActionType
-import com.limor.app.scenes.main.viewmodels.CommentsViewModel
 import com.limor.app.scenes.main.viewmodels.HandleCommentActionsViewModel
-import com.limor.app.scenes.main_new.fragments.DialogPodcastReportP2
+import com.limor.app.uimodels.CastUIModel
 import com.limor.app.uimodels.CommentUIModel
-import com.limor.app.uimodels.UserUIModel
 import dagger.android.support.AndroidSupportInjection
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 class DialogCommentMoreActions : DialogFragment() {
 
     companion object {
-        const val COMMENT_KEY = "comment"
+        const val KEY_COMMENT = "comment"
+        const val KEY_PODCAST = "podcast"
         const val FROM = "from"
         const val ITEM = "item"
-        const val KEY_CAN_EDIT_COMMENT = "KEY_CAN_EDIT_COMMENT"
     }
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val model: HandleCommentActionsViewModel by activityViewModels { viewModelFactory }
     lateinit var binding: DialogCommentMoreActionsBinding
-    lateinit var args: CommentUIModel
+
     lateinit var from: String
     lateinit var actionItem: String
 
-    private var canEdit = false
+    lateinit var comment: CommentUIModel
+    lateinit var cast: CastUIModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DialogCommentMoreActionsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -69,20 +59,35 @@ class DialogCommentMoreActions : DialogFragment() {
     }
 
     private fun setViews() {
-        binding.btnEditComment.visibility = if (canEdit) View.VISIBLE else View.GONE
+        binding.btnEditComment.visibility = if (commentIsEditable()) View.VISIBLE else View.GONE
+
+        binding.btnDeleteComment.visibility = if (isOwnerOfComment() || isOwnerOfPodcast()) View.VISIBLE else View.GONE
+
+        // The visibility of menu items when showing the menu for another user's comment
+        val otherVisibility = if (isOwnerOfComment()) View.GONE else View.VISIBLE
+
+        binding.btnBlockUser.visibility = otherVisibility
+        binding.btnReportUser.visibility = otherVisibility
+        binding.btnReportComment.visibility = otherVisibility
+
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         AndroidSupportInjection.inject(this)
     }
+
     private fun getArgs() {
         arguments?.let {
-            args = it.getParcelable(COMMENT_KEY)!!
+            comment = it.getParcelable(KEY_COMMENT)!!
+            cast = it.getParcelable(KEY_PODCAST)!!
+
             from = it.getString(FROM)!!
             actionItem = it.getString(ITEM)!!
-            canEdit = it.getBoolean(KEY_CAN_EDIT_COMMENT)
-            Timber.d(args.toString())
+
+            if (BuildConfig.DEBUG) {
+                Timber.d("$it")
+            }
         }
     }
 
@@ -91,7 +96,7 @@ class DialogCommentMoreActions : DialogFragment() {
         binding.btnCancel.setOnClickListener { this.dismiss() }
 
         binding.btnEditComment.setOnClickListener {
-            model.commentAction(args, CommentActionType.Edit)
+            model.commentAction(comment, CommentActionType.Edit)
             this.dismiss()
         }
 
@@ -99,25 +104,39 @@ class DialogCommentMoreActions : DialogFragment() {
             when (from) {
                 "comment" -> {
                     when (actionItem) {
-                        "parent" -> model.actionDeleteParentComment(args)
-                        "child" -> model.actionDeleteChildComment(args)
+                        "parent" -> model.actionDeleteParentComment(comment)
+                        "child" -> model.actionDeleteChildComment(comment)
                     }
                 }
                 "reply" -> {
                     when (actionItem) {
-                        "parent" -> model.actionDeleteParentReply(args)
-                        "child" -> model.actionDeleteChildReply(args)
+                        "parent" -> model.actionDeleteParentReply(comment)
+                        "child" -> model.actionDeleteChildReply(comment)
                     }
                 }
             }
 
             dismissAllowingStateLoss()
-
         }
+
         binding.btnReportUser.setOnClickListener {
-            DialogUserReport.newInstance(args.id)
-                .show(parentFragmentManager, DialogUserReport.TAG)
+            DialogUserReport.newInstance(comment.id).show(parentFragmentManager, DialogUserReport.TAG)
             dismiss()
         }
+    }
+
+    private fun commentIsEditable(): Boolean {
+        return isOwnerOfComment() && comment.type == "text"
+    }
+
+    private fun isOwnerOfPodcast(): Boolean {
+        return cast.owner?.id == PrefsHandler.getCurrentUserId(requireContext())
+    }
+
+    private fun isOwnerOfComment(): Boolean {
+        if (BuildConfig.DEBUG) {
+            Timber.d("${comment.user?.id}  -- ${PrefsHandler.getCurrentUserId(requireContext())}")
+        }
+        return comment.user?.id == PrefsHandler.getCurrentUserId(requireContext())
     }
 }
