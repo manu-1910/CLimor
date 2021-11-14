60b9ac360f1123f24a24380387a392981dd6ceae
Vasil Tabakov <vasil@awebdigital.co>
2021-11-14 19:19:42 +0200

    uses limor dialog to warn blocking user

--- app/src/main/kotlin/com/limor/app/scenes/profile/DialogUserProfileActions.kt
+++ app/src/main/kotlin/com/limor/app/scenes/profile/DialogUserProfileActions.kt
@@ -1,31 +1,30 @@
 package com.limor.app.scenes.profile
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.limor.app.R
 import com.limor.app.databinding.DialogOtherUserActionsBinding
 import com.limor.app.scenes.main.fragments.profile.UserProfileActivity
 import com.limor.app.scenes.main.fragments.profile.UserProfileViewModel
-import com.limor.app.scenes.main.fragments.settings.SettingsActivity
-import com.limor.app.scenes.main_new.fragments.DialogPodcastReportP2
+import com.limor.app.scenes.utils.LimorDialog
 import com.limor.app.uimodels.UserUIModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.jetbrains.anko.cancelButton
 import org.jetbrains.anko.okButton
 import org.jetbrains.anko.support.v4.alert
 import timber.log.Timber
-import javax.inject.Inject
 
 class DialogUserProfileActions : DialogFragment() {
 
@@ -34,7 +33,7 @@ class DialogUserProfileActions : DialogFragment() {
     }
     private val model: UserProfileViewModel by viewModels({ activity as UserProfileActivity }) { (activity as UserProfileActivity).viewModelFactory }
     lateinit var binding: DialogOtherUserActionsBinding
-    lateinit var args: UserUIModel
+    lateinit var user: UserUIModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -53,9 +52,9 @@ class DialogUserProfileActions : DialogFragment() {
 
     private fun getArgs() {
         arguments?.let {
-            args = it.getParcelable(USER_KEY)!!
-            Timber.d(args.toString())
-            setUserActions(args)
+            user = it.getParcelable(USER_KEY)!!
+            Timber.d(user.toString())
+            setUserActions(user)
         }
     }
 
@@ -69,24 +68,46 @@ class DialogUserProfileActions : DialogFragment() {
         }
     }
 
+    private fun showBlockDialog() {
+        val userName = user.username ?: ""
+        val message = getString(R.string.confirmation_block_user__with_format, userName)
+        val spannable = SpannableString(message)
+
+        // - 1 because of the question mark at the end
+        val start = message.length - userName.length - 1
+        val end = start + userName.length
+        val flags = Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+
+        // Black
+        spannable.setSpan(ForegroundColorSpan(Color.BLACK), start, end, flags)
+
+        // Bold
+        spannable.setSpan(StyleSpan(Typeface.BOLD), start, end, flags)
+
+        LimorDialog(layoutInflater).apply {
+            setTitle(R.string.block_user)
+            setMessage(spannable)
+            setIcon(R.drawable.ic_block_user_full)
+            addButton(R.string.block, false) { onBlockUser() }
+            addButton(R.string.cancel, true)
+        }.show()
+    }
+
+    private fun onBlockUser() {
+        model.blockUser(user.id)
+        findNavController().previousBackStackEntry?.savedStateHandle?.set("blocked", true)
+        findNavController().popBackStack()
+    }
+
     private fun setOnClicks() {
         binding.btnCancel.setOnClickListener { this.dismiss() }
         binding.btnBlockUser.setOnClickListener {
-            alert(getString(R.string.confirmation_block_user)) {
-                okButton {
-                    model.blockUser(args.id)
-                    findNavController().previousBackStackEntry?.savedStateHandle?.set("blocked", true)
-                    findNavController().popBackStack()
-                }
-                cancelButton {  }
-            }.show()
-
-
+            showBlockDialog()
         }
         binding.btnUnBlockUser.setOnClickListener {
             alert(getString(R.string.confirmation_unblock_user)) {
                 okButton {
-                    model.unblockUser(args.id)
+                    model.unblockUser(user.id)
                     findNavController().previousBackStackEntry?.savedStateHandle?.set("blocked", false)
                     findNavController().popBackStack()
                 }
@@ -96,7 +117,7 @@ class DialogUserProfileActions : DialogFragment() {
         }
 
         binding.btnReportUser.setOnClickListener {
-            DialogUserReport.newInstance(args.id)
+            DialogUserReport.newInstance(user.id)
                 .show(parentFragmentManager, DialogUserReport.TAG)
             dismiss()
         }
