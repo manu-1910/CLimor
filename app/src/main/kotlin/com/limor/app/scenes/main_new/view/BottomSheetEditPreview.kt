f4cd66ef1887d5116be48f4f0bd4bb599a17ec5f
Vasil Tabakov <vasil@awebdigital.co>
2021-11-27 18:29:46 +0200

    initial code for handling the waveform

--- app/src/main/kotlin/com/limor/app/scenes/main_new/view/BottomSheetEditPreview.kt
+++ app/src/main/kotlin/com/limor/app/scenes/main_new/view/BottomSheetEditPreview.kt
@@ -40,15 +40,20 @@ import kotlin.math.sqrt
 class BottomSheetEditPreview : BottomSheetDialogFragment() {
 
     private val castId: Int by lazy { requireArguments()[CAST_ID] as Int }
-    private val castURL: String by lazy { requireArguments()[CAST_URL] as String }
+    private val castURL: String by lazy {
+        "https://limor-platform-development.s3-eu-west-1.amazonaws.com/podcast_audio_direct_upload/audioFile_163661872969286112_1636618729691.x-m4a"
+        // requireArguments()[CAST_URL] as String
+    }
 
     private lateinit var playVisualiser: PlayVisualizer
     private lateinit var rewindButton: ImageButton
     private lateinit var forwardButton: ImageButton
     private lateinit var playButton: ImageButton
     private lateinit var saveButton: Button
-    private lateinit var destinationFile: File
+
     private lateinit var progressBar: ProgressBar
+
+    private var destinationFile: File? = null
 
     val mediaPlayer = MediaPlayer()
 
@@ -57,8 +62,12 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
         setStyle(STYLE_NORMAL, R.style.CustomBottomSheet)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.sheet_edit_preview, container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.sheet_edit_preview, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -92,7 +101,7 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
         }
     }
 
-    private fun initialiseViews(view: View){
+    private fun initialiseViews(view: View) {
         playVisualiser = view.findViewById(R.id.playVisualizer)
         rewindButton = view.findViewById(R.id.rewindButton)
         forwardButton = view.findViewById(R.id.forwardButton)
@@ -101,7 +110,7 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
         progressBar = view.findViewById(R.id.progressBar)
     }
 
-    private fun setClickListeners(){
+    private fun setClickListeners() {
         rewindButton.setOnClickListener {
 
         }
@@ -118,61 +127,7 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
                     )
                 )
             } else {
-                lifecycleScope.launch {
-                    mediaPlayer.setDataSource(destinationFile.path)
-                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-
-                    val seekHandler: Handler = Handler(Looper.getMainLooper())
-                    var seekUpdater: Runnable = object : Runnable {
-                        override fun run() {
-                            seekHandler.postDelayed(this, 100)
-                            mediaPlayer.let {
-                                if (it.isPlaying) {
-                                    val currentPosition = it.currentPosition
-                                    playVisualiser.updateTime(currentPosition.toLong(), true)
-                                }
-                            }
-                        }
-                    }
-
-                    val mRecorder = WaveRecorder(destinationFile.path)
-                    mRecorder.waveConfig.sampleRate = 44100
-                    mRecorder.waveConfig.channels = AudioFormat.CHANNEL_IN_STEREO
-                    mRecorder.waveConfig.audioEncoding = AudioFormat.ENCODING_PCM_16BIT
-                    val amps: List<Int> = loadAmps(destinationFile.path, mRecorder.bufferSize)
-
-                    mediaPlayer.prepareAsync()
-                    playVisualiser.visibility = View.VISIBLE
-
-                    mediaPlayer.setOnCompletionListener {
-                        playVisualiser.updateTime(mediaPlayer.duration.toLong(), false)
-                        it.pause()
-                        playButton.setImageDrawable(
-                            ContextCompat.getDrawable(
-                                requireContext(),
-                                R.drawable.ic_play
-                            )
-                        )
-                    }
-                    mediaPlayer.setOnPreparedListener {
-                        it.start()
-                        playButton.setImageDrawable(
-                            ContextCompat.getDrawable(
-                                requireContext(),
-                                R.drawable.ic_pause
-                            )
-                        )
-                        seekHandler.post(seekUpdater)
-                    }
-
-                    playVisualiser.apply {
-                        ampNormalizer = { sqrt(it.toFloat()).toInt() }
-                    }
-                    playVisualiser.setWaveForm(
-                        amps,
-                        mRecorder.tickDuration
-                    )
-                }
+                playAudio()
             }
         }
         saveButton.setOnClickListener {
@@ -187,17 +142,60 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
         }
     }
 
-    private fun showProgress(){
+    private fun playAudio() {
+        val audioFile = destinationFile ?: return
+
+        mediaPlayer.setDataSource(audioFile.path)
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+
+        val seekHandler: Handler = Handler(Looper.getMainLooper())
+        var seekUpdater: Runnable = object : Runnable {
+            override fun run() {
+                seekHandler.postDelayed(this, 100)
+                mediaPlayer.let {
+                    if (it.isPlaying) {
+                        val currentPosition = it.currentPosition
+                        playVisualiser.updateTime(currentPosition.toLong(), true)
+                    }
+                }
+            }
+        }
+
+        mediaPlayer.prepareAsync()
+
+        mediaPlayer.setOnCompletionListener {
+            playVisualiser.updateTime(mediaPlayer.duration.toLong(), false)
+            it.pause()
+            playButton.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_play
+                )
+            )
+        }
+        mediaPlayer.setOnPreparedListener {
+            it.start()
+            playButton.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_pause
+                )
+            )
+            seekHandler.post(seekUpdater)
+        }
+    }
+
+    private fun showProgress() {
         progressBar.visibility = View.VISIBLE
         playButton.visibility = View.INVISIBLE
     }
 
-    private fun hideProgress(){
+    private fun hideProgress() {
         progressBar.visibility = View.GONE
         playButton.visibility = View.VISIBLE
     }
 
-    private fun downloadCast(){
+    private fun downloadCast() {
         if (castURL.isNotEmpty()) {
             val downloadDirectory =
                 File(requireContext().getExternalFilesDir(null)?.absolutePath, "/limorv2/download/")
@@ -209,40 +207,68 @@ class BottomSheetEditPreview : BottomSheetDialogFragment() {
             destinationFile = File(finalPath)
             showProgress()
             doAsync {
-                try{
+                try {
                     URL(castURL).openStream().use { input ->
                         FileOutputStream(destinationFile).use { output ->
                             input.copyTo(output)
-                            runOnUiThread {
-                                hideProgress()
+                            lifecycleScope.launch {
+                                onFileDownloaded()
                             }
                         }
                     }
-                }
-                catch (e: Exception){
-                    Log.d("sdvf",e.toString())
+                } catch (e: Exception) {
+                    // TODO show error ?
+                    Log.d("sdvf", e.toString())
                 }
             }
         }
     }
 
-    @Suppress("BlockingMethodInNonBlockingContext")
-    suspend fun loadAmps(recordFile: String, bufferSize: Int): List<Int> = withContext(Dispatchers.IO) {
-        val amps = mutableListOf<Int>()
-        val buffer = ByteArray(bufferSize)
-        File(recordFile).inputStream().use {
-            it.skip(44.toLong())
-
-            var count = it.read(buffer)
-            while (count > 0) {
-                amps.add(buffer.calculateAmplitude())
-                count = it.read(buffer)
-            }
+    private suspend fun onFileDownloaded() {
+        val audioFile = destinationFile ?: return
+        if (!audioFile.exists()) {
+            // TODO show errors ?
+            dismiss()
+            return;
         }
-        amps
+        val mRecorder = WaveRecorder(audioFile.path)
+        mRecorder.waveConfig.sampleRate = 44100
+        mRecorder.waveConfig.channels = AudioFormat.CHANNEL_IN_STEREO
+        mRecorder.waveConfig.audioEncoding = AudioFormat.ENCODING_PCM_16BIT
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val amps: List<Int> = loadAmps(audioFile.path, mRecorder.bufferSize)
+            playVisualiser.setWaveForm(
+                amps,
+                mRecorder.tickDuration
+            )
+        }
+
+        playVisualiser.apply {
+            ampNormalizer = { sqrt(it.toFloat()).toInt() }
+            visibility = View.VISIBLE
+        }
+
+        hideProgress();
     }
 
-    companion object{
+    suspend fun loadAmps(recordFile: String, bufferSize: Int): List<Int> =
+        withContext(Dispatchers.IO) {
+            val amps = mutableListOf<Int>()
+            val buffer = ByteArray(bufferSize)
+            File(recordFile).inputStream().use {
+                it.skip(44.toLong())
+
+                var count = it.read(buffer)
+                while (count > 0) {
+                    amps.add(buffer.calculateAmplitude())
+                    count = it.read(buffer)
+                }
+            }
+            amps
+        }
+
+    companion object {
         const val CAST_ID = "CAST_ID"
         const val CAST_URL = "CAST_URL"
         const val TAG = "EDIT_PREVIEW_SHEET"
