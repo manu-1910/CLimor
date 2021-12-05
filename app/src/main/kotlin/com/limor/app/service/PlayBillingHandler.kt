80518758e9df672ed5244a52cc8cfe14b92a4f20
Vasil Tabakov <vasil@awebdigital.co>
2021-12-05 16:27:22 +0200

    initial logic for fetching skudetails

--- app/src/main/kotlin/com/limor/app/service/PlayBillingHandler.kt
+++ app/src/main/kotlin/com/limor/app/service/PlayBillingHandler.kt
@@ -2,18 +2,42 @@ package com.limor.app.service
 
 import android.app.Activity
 import android.content.Context
+import android.os.Looper
 import com.android.billingclient.api.*
-import com.limor.app.scenes.auth_new.util.PrefsHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.limor.app.BuildConfig
+import com.limor.app.apollo.PublishRepository
+import kotlinx.coroutines.*
 import timber.log.Timber
+import java.lang.RuntimeException
+import java.lang.ref.WeakReference
 import javax.inject.Inject
 import javax.inject.Singleton
 
+interface DetailsAvailableListener {
+    fun onDetailsAvailable(details: Map<String, SkuDetails>)
+}
+
+interface ProductDetails {
+    fun getPrice(productId: String, listener: DetailsAvailableListener)
+}
+
 @Singleton
-class PlayBillingHandler @Inject constructor(private val context: Context) {
+class PlayBillingHandler @Inject constructor(
+    private val context: Context,
+    private val publishRepository: PublishRepository,
+) : ProductDetails {
 
     lateinit var handlePurchases: (Purchase) -> Unit
+
+    private val billingJob = SupervisorJob()
+    private val billingScope = CoroutineScope(Dispatchers.IO + billingJob)
+
+    private val productSkuDetails = mutableMapOf<String, SkuDetails>()
+
+    private var lastFetchTime = 0L
+
+    private val detailsListeners = mutableListOf<WeakReference<DetailsAvailableListener>>()
+
     private val purchasesUpdatedListener by lazy {
         PurchasesUpdatedListener { billingResult, purchases ->
             //This is called once there is some update about purchase after launching the billing flow
@@ -31,6 +55,7 @@ class PlayBillingHandler @Inject constructor(private val context: Context) {
         }
 
     }
+
     private val billingClient by lazy {
         BillingClient.newBuilder(context)
             .enablePendingPurchases()
@@ -38,52 +63,32 @@ class PlayBillingHandler @Inject constructor(private val context: Context) {
             .build()
     }
 
-    suspend fun querySUBSKUDetails(
-        skuIds: ArrayList<String>,
-        handlePurchase: (Purchase) -> Unit,
-    ): List<SkuDetails>? {
-        val params = SkuDetailsParams.newBuilder()
-        params.setSkusList(skuIds)
-            .setType(BillingClient.SkuType.SUBS)
-        handlePurchases = handlePurchase
-        return getSkusFromParams(params).skuDetailsList
+    private suspend fun queryInAppSKUDetails(skuIds: List<String>): List<SkuDetails> {
+
+        // unfortunately, Android has an undocumented limit on the size of the list in this request.
+        // 20 is a number used in one of the Google samples, namely "Trivial Drive", source code of which
+        // can be found here:
+        // https://github.com/googlesamples/android-play-billing/blob/master/TrivialDrive/app/src/main/java/com/example/android/trivialdrivesample/util/IabHelper.java
+        // more info here as well:
+        // https://github.com/serso/android-checkout/issues/56
+
+        return skuIds
+            .chunked(20)
+            .map {
+                SkuDetailsParams.newBuilder()
+                    .setSkusList(skuIds)
+                    .setType(BillingClient.SkuType.INAPP)
+            }.map {
+                getSkusFromParams(it)
+            }.flatten()
     }
 
-    suspend fun queryInAppSKUDetails(
-        skuIds: ArrayList<String>,
-        handlePurchase: (Purchase) -> Unit,
-    ): List<SkuDetails>? {
-        val params = SkuDetailsParams.newBuilder()
-        params.setSkusList(skuIds).setType(BillingClient.SkuType.INAPP)
-        handlePurchases = handlePurchase
-        return getSkusFromParams(params).skuDetailsList
-    }
-
-    private suspend fun getSkusFromParams(params: SkuDetailsParams.Builder): SkuDetailsResult {
-        val skuDetailsResult = withContext(Dispatchers.IO) {
-            /*billingClient.queryPurchasesAsync(BillingClient.SkuType.INAPP,object : PurchasesResponseListener{
-                override fun onQueryPurchasesResponse(
-                    p0: BillingResult,
-                    p1: MutableList<Purchase>,
-                ) {
-                    p1.forEach{
-                        purchase ->
-                        val consumeParams =
-                            ConsumeParams.newBuilder()
-                                .setPurchaseToken(purchase.purchaseToken)
-                                .build()
-                    }
-                }
-            })*/
-            billingClient.querySkuDetails(params.build())
-
+    private suspend fun getSkusFromParams(params: SkuDetailsParams.Builder): List<SkuDetails> {
+        val result = billingClient.querySkuDetails(params.build())
+        if (BuildConfig.DEBUG) {
+            Timber.d("Billing SKUs-> ${result.skuDetailsList}")
         }
-
-        Timber.d("Billing SKUs-> ${skuDetailsResult.skuDetailsList}")
-        skuDetailsResult.skuDetailsList?.forEach { item ->
-            PrefsHandler.saveSkuDetails(context, item.sku, item)
-        }
-        return skuDetailsResult
+        return result.skuDetailsList ?: listOf()
     }
 
     fun close() {
@@ -121,5 +126,86 @@ class PlayBillingHandler @Inject constructor(private val context: Context) {
         }
     }
 
+    private suspend fun fetchProducts() {
+        // TODO use a constant for the type
+        val productIds = publishRepository.getInAppPricesTiers("castPriceTiers")
+        if (BuildConfig.DEBUG) {
+            Timber.d("Product IDs from backend: $productIds")
+        }
+        val details = queryInAppSKUDetails(productIds)
+        if (BuildConfig.DEBUG) {
+            Timber.d("Got SKU Details -> $details")
+        }
+    }
+
+    /**
+     * This connects to the billing client if not connected and fetches the product IDs.
+     */
+    private fun fetchProductIDs() {
+        lastFetchTime = System.currentTimeMillis()
+        connectToBillingClient { connected ->
+            if (connected) {
+                billingScope.launch {
+                    fetchProducts()
+                    println("Thread is main 1 -> ${Looper.getMainLooper().isCurrentThread}")
+                }
+                println("Thread is main 2 -> ${Looper.getMainLooper().isCurrentThread}")
+            }
+        }
+    }
+
+    private fun onListener(detailsAvailableListener: DetailsAvailableListener) {
+        for (listener in detailsListeners) {
+            // Listener already in the list of listeners
+            if (listener.get() == detailsAvailableListener) {
+                return
+            }
+        }
+        detailsListeners.add(WeakReference(detailsAvailableListener))
+    }
+
+    private fun notifyListeners() {
+        for (listener in detailsListeners) {
+            listener.get()?.onDetailsAvailable(productSkuDetails)
+        }
+    }
+
+    /**
+     * This ensures the PlayBillingHandler has fresh details for all product details and in
+     * particular for the specified productId. We pass the productId so that the billing client
+     * can fetch the prices again if a new product was added on Google Play and thus that productId
+     * isn't available in the cached data.
+     *
+     * This should only be called on the main thread otherwise it throws an exception.
+     */
+    override fun getPrice(productId: String, listener: DetailsAvailableListener) {
+        if (!Looper.getMainLooper().isCurrentThread) {
+            throw RuntimeException("getPrice must be called on the main thread.")
+        }
+
+        onListener(listener)
+
+        // Always refresh the product IDs if the refresh time limit is reached
+        //
+        val delta = System.currentTimeMillis() - lastFetchTime
+        if (delta > refreshTimeLimit) {
+            fetchProductIDs()
+            return
+        }
+
+        if (productSkuDetails.containsKey(productId)) {
+            listener.onDetailsAvailable(productSkuDetails)
+            return
+        }
+
+        // At this point the passed product ID does not exist in the cached list so we refresh
+        // the list
+        fetchProductIDs()
+    }
+
+    companion object {
+        // 1 day
+        private const val refreshTimeLimit = 1 * 24 * 60 * 60 * 1000L
+    }
 
 }
