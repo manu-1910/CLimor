d402bda750013b07404519c3d95909a78b82f905
Vasil Tabakov <vasil@awebdigital.co>
2021-12-10 11:31:37 +0200

    debug log to debug update preview issues

--- app/src/main/kotlin/com/limor/app/usecases/UpdatePreviewUseCase.kt
+++ app/src/main/kotlin/com/limor/app/usecases/UpdatePreviewUseCase.kt
@@ -1,8 +1,10 @@
 package com.limor.app.usecases
 
+import com.limor.app.BuildConfig
 import com.limor.app.apollo.PodcastInteractionsRepository
 import com.limor.app.common.dispatchers.DispatcherProvider
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import javax.inject.Inject
 
 class UpdatePreviewUseCase @Inject constructor(
@@ -11,6 +13,9 @@ class UpdatePreviewUseCase @Inject constructor(
 ) {
     suspend fun execute(podcastId: Int, previewDuration: Int, startsAt: Int, endsAt: Int): Boolean {
         var success = false
+        if (BuildConfig.DEBUG) {
+            Timber.d("Updating the preview for podcast ID $podcastId, starting at $startsAt and ending at $endsAt with a duration of $previewDuration");
+        }
         withContext(dispatcherProvider.io) {
             success = repository.updatePreview(podcastId, previewDuration, startsAt, endsAt)
         }
