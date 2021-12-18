e3a1f4caf97b4a3034c588097159d6a53a96bcf3
Vasil Tabakov <vasil@awebdigital.co>
2021-12-18 14:11:29 +0200

    fixes a phone auth edge case reported in crashlytics

--- app/src/main/kotlin/com/limor/app/scenes/auth_new/firebase/PhoneAuthHandler.kt
+++ app/src/main/kotlin/com/limor/app/scenes/auth_new/firebase/PhoneAuthHandler.kt
@@ -154,6 +154,12 @@ class PhoneAuthHandler @Inject constructor() :
     }
 
     fun enterCodeAndSignIn(code: String) {
+        if (code.isEmpty()) {
+            activity?.let {
+                _smsCodeValidationErrorMessage.postValue(it.getString(R.string.no_code_entered))
+            }
+            return
+        }
         scope.launch {
             // this would enable the 'Continue' button in the FragmentVerifyPhoneNumber, which we
             // don't need as it's confusing you can continue while the phone verification is
