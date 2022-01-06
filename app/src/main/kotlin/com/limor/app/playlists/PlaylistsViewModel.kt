f1e0a44f7c8e5d122e19d08cc0c95a09da47e7ab
Vasil Tabakov <vasil@awebdigital.co>
2022-01-06 17:20:13 +0200

    view model returns dummy data

--- app/src/main/kotlin/com/limor/app/playlists/PlaylistsViewModel.kt
+++ app/src/main/kotlin/com/limor/app/playlists/PlaylistsViewModel.kt
@@ -1,7 +1,19 @@
 package com.limor.app.playlists
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.limor.app.playlists.models.PlaylistUIModel
+import javax.inject.Inject
 
-class PlaylistsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class PlaylistsViewModel @Inject constructor(
+
+): ViewModel() {
+
+    fun getPlaylists(): LiveData<List<PlaylistUIModel>> {
+        val liveData = MutableLiveData<List<PlaylistUIModel>>()
+        liveData.postValue(PlaylistUIModel.dummyList(10))
+        return liveData
+    }
+
 }
