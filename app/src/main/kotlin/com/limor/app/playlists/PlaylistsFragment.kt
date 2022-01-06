6040e1fbd1b2beacd2021d0d6e0d16b789f5e589
Vasil Tabakov <vasil@awebdigital.co>
2022-01-06 17:21:41 +0200

    sets the playlist adapter with dummy data

--- app/src/main/kotlin/com/limor/app/playlists/PlaylistsFragment.kt
+++ app/src/main/kotlin/com/limor/app/playlists/PlaylistsFragment.kt
@@ -9,17 +9,18 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import com.limor.app.R
+import com.limor.app.common.BaseFragment
+import com.limor.app.databinding.FragmentShareDialogBinding
+import com.limor.app.databinding.PlaylistsFragmentBinding
+import com.limor.app.playlists.adapter.PlaylistsAdapter
+import com.limor.app.playlists.models.PlaylistUIModel
 import com.limor.app.uimodels.UserUIModel
 import javax.inject.Inject
 
-class PlaylistsFragment : Fragment() {
+class PlaylistsFragment : BaseFragment() {
 
-    companion object {
-        private const val KEY_USER = "KEY_USER"
-        fun newInstance(user: UserUIModel) = PlaylistsFragment().apply {
-            arguments = bundleOf(KEY_USER to user)
-        }
-    }
+    private var _binding: PlaylistsFragmentBinding? = null
+    private val binding get() = _binding!!
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -28,8 +29,37 @@ class PlaylistsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.playlists_fragment, container, false)
+    ): View {
+        val binding = PlaylistsFragmentBinding.inflate(inflater, container, false);
+        _binding = binding
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        playlistsViewModel.getPlaylists().observe(viewLifecycleOwner) { playlists ->
+            showPlaylists(playlists)
+        }
+    }
+
+    private fun showPlaylists(playlists: List<PlaylistUIModel>) {
+        PlaylistsAdapter(
+            onPlaylistClick = { playlist ->
+
+            },
+            onDeleteClick = { playlist ->
+
+            }
+        ).also {
+            it.submitList(playlists)
+            binding.recyclerPlaylists.adapter = it
+        }
+    }
+
+    companion object {
+        private const val KEY_USER = "KEY_USER"
+        fun newInstance(user: UserUIModel) = PlaylistsFragment().apply {
+            arguments = bundleOf(KEY_USER to user)
+        }
+    }
 }
