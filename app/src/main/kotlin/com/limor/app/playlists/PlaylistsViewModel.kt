a1e8338caa21defb49ebc6ebdc7297d7449557dc
Manohar195 <manohar@awebdigital.co>
2022-01-13 10:38:39 +0530

    Defined api call for get all casts in playlist

--- app/src/main/kotlin/com/limor/app/playlists/PlaylistsViewModel.kt
+++ app/src/main/kotlin/com/limor/app/playlists/PlaylistsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.limor.app.apollo.CastsRepository
+import com.limor.app.playlists.models.PlaylistCastUIModel
 import com.limor.app.playlists.models.PlaylistUIModel
 import com.limor.app.playlists.models.mapToUIModel
 import kotlinx.coroutines.launch
@@ -28,6 +29,16 @@ class PlaylistsViewModel @Inject constructor(
         val list = ArrayList<PlaylistUIModel>()
         list.addAll(PlaylistUIModel.dummyList(10))
         liveData.postValue(list)
+        return liveData
+    }
+
+    fun getCastsInPlaylist(playlistId: Int): LiveData<List<PlaylistCastUIModel>> {
+        val liveData = MutableLiveData<List<PlaylistCastUIModel>>()
+        viewModelScope.launch {
+            val casts = castsRepository.getCastsInPlaylist(playlistId)
+                .mapNotNull { it -> it?.mapToUIModel() }
+            liveData.postValue(casts)
+        }
         return liveData
     }
 
