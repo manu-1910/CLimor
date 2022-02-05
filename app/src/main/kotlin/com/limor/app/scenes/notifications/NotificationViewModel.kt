95f91af800990e2cfc4f427110c9e56c4d0a42d8
Vasil Tabakov <vasil@awebdigital.co>
2022-02-05 15:39:14 +0200

    get notifications flow

--- app/src/main/kotlin/com/limor/app/scenes/notifications/NotificationViewModel.kt
+++ app/src/main/kotlin/com/limor/app/scenes/notifications/NotificationViewModel.kt
@@ -4,14 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.limor.app.apollo.GeneralInfoRepository
+import com.limor.app.scenes.main_new.pagingsources.NotificationsPagingSource
 import com.limor.app.uimodels.NotiUIMode
 import com.limor.app.usecases.GetNotificationUserCaseNew
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 class NotificationViewModel @Inject constructor(
-    private val getNotificationUserCase: GetNotificationUserCaseNew
+    private val getNotificationUserCase: GetNotificationUserCaseNew,
+    private val repository: GeneralInfoRepository,
 ): ViewModel() {
 
     private val _noti = MutableLiveData<List<NotiUIMode>>()
@@ -28,6 +36,13 @@ class NotificationViewModel @Inject constructor(
                     Timber.e(it, "Error while fetching notifications")
                 }
         }
+    }
+
+    fun getNotifications(): Flow<PagingData<NotiUIMode>> {
+        return Pager(
+            config = PagingConfig(pageSize = NotificationsPagingSource.NETWORK_PAGE_SIZE),
+            pagingSourceFactory = { NotificationsPagingSource(repository = repository) }
+        ).flow.cachedIn(viewModelScope)
     }
 
     fun updateReadStatus(nId: Int, read: Boolean) {
